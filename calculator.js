2d183327a652b58d89a7d9572518220156cd74c8
vishal-s-patel <vishalpatel1002@yahoo.com>
2025-10-02 14:01:08 +0530

    fix: step 1 is completed.

--- calculator.js
+++ calculator.js
@@ -2,6 +2,15 @@
     @param {string}  numbers
     @return {number} sum of numbers
  */
-function add(numbers) {}
+function add(numbers) {
+  if (numbers === "") {
+    return 0;
+  }
+  if (numbers.includes(",")) {
+    const numArray = numbers.split(",").map(Number);
+    return numArray.reduce((acc, curr) => acc + curr, 0);
+  }
+  return parseInt(numbers);
+}
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,11 @@ const add = require("./calculator");
 test("for an empty string it will return 0", () => {
   expect(add("")).toBe(0);
 });
+
+test("for a single number it will return the value", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("for two numbers it will return the sum", () => {
+  expect(add("1,2")).toBe(3);
+});
